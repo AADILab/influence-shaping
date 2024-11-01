73b177e4562cd80ff4f66024cb03c4bc338de6c4
EverardoG <everardo.a.gonzalez@gmail.com>
2024-11-01 15:14:03 -0700

    unit tests for G

--- tests/test_env.py
+++ tests/test_env.py
@@ -13,7 +13,14 @@ python -m unittest tests.test_env.TestEnv.test_env
 
 import unittest
 import numpy as np
+from influence.librovers import rovers
 from influence.custom_env import createEnv
+
+# Helper functions
+def distance(entity_0, entity_1):
+    return np.sqrt((entity_0.position().x - entity_1.position().x)**2 + (entity_0.position().y - entity_1.position().y)**2)
+def compute_poi_reward(poi, agent):
+    return 1.0 / max(distance(poi, agent), 1.0)
 
 class TestEnv(unittest.TestCase):
     def test_env(self):
@@ -150,59 +157,188 @@ class TestEnv(unittest.TestCase):
                 self.assertTrue(np.isclose(expected_observation[i], observation[i,0]))
 
         # Check the rewards
-        def distance(entity_0, entity_1):
-            return np.sqrt((entity_0.position().x - entity_1.position().x)**2 + (entity_0.position().y - entity_1.position().y)**2)
-        def compute_poi_reward(poi, agent):
-            return 1.0 / max(distance(poi, agent), 1.0)
         expected_G = compute_poi_reward(pois[0], agents[0]) + compute_poi_reward(pois[1], agents[1])
         for agent_reward in rewards:
             self.assertTrue(np.isclose(expected_G, agent_reward), f"{expected_G} != {agent_reward}")
 
     def test_G(self):
-        """Check that G is computed as expected for different
+        """Check that G is computed as expected for different cases
+        - additional rovers with low coupling (if coupling is satisfied, addtl rovers shouldn't change anything)
         - poi observation radii
-        - rover observation radii
-        - coupling requirements
         - uavs in the environment (should not affect G)
+        - more pois
         """
-        pass
-        # Put just one poi and one rover
-#         rover_pos = [24.0, 25.0]
-#         poi_pos = [24.0, 24.0]
-#         config = {
-#             'env': {
-#                 'agents': {
-#                     'rovers': [
-#                         {
-#                             'observation_radius': 1000.0,
-#                             'position': {
-#                                 'spawn_rule': 'fixed',
-#                                 'fixed': agent_0_pos
-#                             },
-#                             'resolution': 90,
-#                             'reward_type': 'Global'
-#                         }
-#                     ],
-#                     'uavs': []
-#                 },
-#                 'pois': {
-#                     'rover_pois': [
-#                         {
-#                             'coupling': 1,
-#                             'observation_radius': 1000.0,
-#                             'position': {
-#                                 'spawn_rule': 'fixed',
-#                                 'fixed': poi_0_pos
-#                             },
-#                             'value': 1.0
-#                         }
-#                     ],
-#                     'hidden_pois': []
-#                 },
-#                 'map_size': [50., 50.]
-#             }
-#         }
+        def compute_G(env):
+            return env.rovers()[0].reward(rovers.AgentPack(0, env.rovers(), env.pois()))
+        def get_default_rover_config():
+            return {
+                'observation_radius': 1000.0,
+                'position': {
+                    'spawn_rule': 'fixed',
+                    'fixed': [25.0, 24.0]
+                },
+                'resolution': 90,
+                'reward_type': 'Global'
+            }
+        def get_default_uav_config():
+            return {
+                'observation_radius': 1000.0,
+                'position': {
+                    'spawn_rule': 'fixed',
+                    'fixed': [23.0, 24.0]
+                },
+                'resolution': 90,
+                'reward_type': 'Global'
+            }
+        def get_default_poi_config():
+            return {
+                'coupling': 1,
+                'observation_radius': 1000.0,
+                'position': {
+                    'spawn_rule': 'fixed',
+                    'fixed': [24.0, 24.0]
+                },
+                'value': 1.0
+            }
+        def get_default_config():
+            return {
+                'env': {
+                    'agents': {
+                        'rovers': [
+                            get_default_rover_config()
+                        ],
+                        'uavs': []
+                    },
+                    'pois': {
+                        'rover_pois': [
+                           get_default_poi_config() 
+                        ],
+                        'hidden_pois': []
+                    },
+                    'map_size': [50., 50.]
+                }
+            }
+        # Check one rover, one poi. Reward of 1.0
+        config = get_default_config()
+        env = createEnv(config)
+        _, (reward) = env.reset()
+        expected_reward = 1.0
+        self.assertTrue(np.isclose(reward, expected_reward))
 
+        # Place the rover closer to the poi. Reward should still be 1.0
+        # env.rovers()[0].set_position(24.5, 24.0)
+        config['env']['agents']['rovers'][0]['position']['fixed'] = [24.5, 24.0]
+        env = createEnv(config)
+        _, (reward) = env.reset()
+        expected_reward = 1.0
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Place the rover further away. Reward should be 0.5
+        config['env']['agents']['rovers'][0]['position']['fixed'] = [26.0, 24.0]
+        env = createEnv(config)
+        _, (reward) = env.reset()
+        expected_reward = 0.5
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Make the rover follow a path. 
+        # Reward should be 0.5 based on final position in path to poi
+        rover_path = [
+            [24.5, 24.0],
+            [26.0, 24.0]
+        ]
+        config = get_default_config()
+        _, _ = env.reset()
+        for position in rover_path:
+            env.rovers()[0].set_position(position[0], position[1])
+        reward = compute_G(env)
+        expected_reward = 0.5
+        self.assertTrue(np.isclose(reward, expected_reward), f'{reward} != {expected_reward}')
+
+        # Now let's add another copy of our rover to make sure we are not double counting
+        config['env']['agents']['rovers'].append(get_default_rover_config())
+        env = createEnv(config)
+        _, (reward, _) = env.reset()
+        expected_reward = 1.0
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Add another 10 and check again for good measure
+        for _ in range(10):
+            config['env']['agents']['rovers'].append(get_default_rover_config())
+        env = createEnv(config)
+        _, rewards = env.reset()
+        reward = rewards[0]
+        expected_reward = 1.0
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Reset the config, and bring down the observation radius for the poi
+        # G should still be 1.0
+        config = get_default_config()
+        config['env']['pois']['rover_pois'][0]['observation_radius'] = 5.0
+        env = createEnv(config)
+        _, (reward) = env.reset()
+        expected_reward = 1.0
+        self.assertTrue(np.isclose(reward, expected_reward))
+        
+        # But if the rover moves away, G should go down
+        env.rovers()[0].set_position(27.0, 24.0)
+        reward = compute_G(env)
+        expected_reward = compute_poi_reward(env.pois()[0], env.rovers()[0])
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # If the rover moves far enough away, G becomes 0.0
+        env.rovers()[0].set_position(30.0, 24.0)
+        reward = compute_G(env)
+        expected_reward = 0.0
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Let's set the default config and throw in some uavs. Should not change G
+        config = get_default_config()
+        expected_reward = 1.0
+        for _ in range(3):
+            config['env']['agents']['uavs'].append(get_default_uav_config())
+            env = createEnv(config)
+            _, rewards = env.reset()
+            reward = rewards[0]
+            self.assertTrue(np.isclose(reward, expected_reward))
+        
+        # Let's set the default config and add more pois.
+        config = get_default_config()
+        more_poi_positions = [
+            [25.0, 35.0],
+            [35.0, 35.0]
+        ]
+        poi_config = get_default_poi_config()
+        for position in more_poi_positions:
+            poi_config['position']['fixed'] = position
+            config['env']['pois']['rover_pois'].append(poi_config)
+        # With only 1 rover, G should be 1.0 + the value of the other two pois
+        env = createEnv(config)
+        reward = compute_G(env)
+        expected_reward = 1.0 + \
+            compute_poi_reward(env.pois()[1], env.rovers()[0]) + \
+            compute_poi_reward(env.pois()[2], env.rovers()[0])
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Let's add uavs to observe the remaining pois 
+        # (reward should not change because uavs can't observe pois)
+        uav_config = get_default_uav_config()
+        for position in more_poi_positions:
+            uav_config['position']['fixed'] = position
+        env = createEnv(config)
+        reward = compute_G(env)
+        expected_reward = 1.0 + \
+            compute_poi_reward(env.pois()[1], env.rovers()[0]) + \
+            compute_poi_reward(env.pois()[2], env.rovers()[0])
+        self.assertTrue(np.isclose(reward, expected_reward))
+
+        # Now let's add rovers to actually observe the remaining pois
+        rover_config = get_default_rover_config()
+        for position in more_poi_positions:
+            rover_config['position']['fixed'] = position
+        env = createEnv(config)
+        reward = compute_G(env)
+        expected_reward = 3.0
+        self.assertTrue(np.isclose(reward, expected_reward))
 
 if __name__ == '__main__':
     unittest.main()
