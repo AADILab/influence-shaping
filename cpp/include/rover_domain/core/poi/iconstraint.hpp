fe31e993be3f1793bb4ae49fdadc6b87c0a7dee9
EverardoG <everardo.a.gonzalez@gmail.com>
2026-01-26 15:39:47 -0800

    remove pack from iconstraint

--- cpp/include/rover_domain/core/poi/iconstraint.hpp
+++ cpp/include/rover_domain/core/poi/iconstraint.hpp
@@ -1,7 +1,6 @@
 #ifndef THYME_ENVIRONMENTS_ROVER_DOMAIN_POI_ICONSTRAINT
 #define THYME_ENVIRONMENTS_ROVER_DOMAIN_POI_ICONSTRAINT
 
-// #include <rover_domain/core/detail/pack.hpp>
 #include <rover_domain/core/rover/rover.hpp>
 #include <rover_domain/core/detail/agent_types.hpp>
 #include <rover_domain/core/detail/entity_types.hpp>
@@ -47,24 +46,24 @@ class AbstractRoverConstraint : public IConstraint {
         return false;
     }
 
-    [[nodiscard]] double step_is_satisfied(const POIPack& entity_pack, std::size_t t) const {
+    [[nodiscard]] double step_is_satisfied(const POIs& pois, const Agents& agents, int poi_idx, std::size_t t) const {
         int count = 0;
         std::vector<double> dists;
         bool constraint_satisfied = false;
 
-        for (const auto& agent : entity_pack.agents) {
+        for (const auto& agent : agents) {
             if (agent->type() == "rover") {
                 double dist;
                 // Check for counterfactual removal (position [-1, -1])
                 if (agent->path()[t].x == -1 && agent->path()[t].y == -1) {
                     dist = std::numeric_limits<double>::infinity();
                 } else {
-                    dist = thyme::math::l2_norm(agent->path()[t], entity_pack.entity->position());
+                    dist = thyme::math::l2_norm(agent->path()[t], pois[poi_idx]->position());
                 }
 
                 dists.push_back(dist);
 
-                if (captured(dist, agent, entity_pack.entity)) {
+                if (captured(dist, agent, pois[poi_idx])) {
                     count++;
                     if (count >= m_coupling) {
                         constraint_satisfied = true;
@@ -119,7 +118,7 @@ class RoverConstraint : public AbstractRoverConstraint {
 
         // Get final timestep
         std::size_t t_final = agents[0]->path().size() - 1;
-        return step_is_satisfied(POIPack(pois[poi_idx], agents, pois), t_final);
+        return step_is_satisfied(pois, agents, poi_idx, t_final);
     }
 };
 
@@ -146,7 +145,7 @@ class RoverSequenceConstraint : public AbstractRoverConstraint {
         std::size_t path_size = agents[0]->path().size();
 
         for (std::size_t t = 0; t < path_size; ++t) {
-            steps.push_back(step_is_satisfied(POIPack(pois[poi_idx], agents, pois), t));
+            steps.push_back(step_is_satisfied(pois, agents, poi_idx, t));
         }
 
         return *std::max_element(steps.begin(), steps.end());
