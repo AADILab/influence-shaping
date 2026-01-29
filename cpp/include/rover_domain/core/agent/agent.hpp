ac57cda2a2d94452bc6985ffd75120d5fefe4f99
EverardoG <everardo.a.gonzalez@gmail.com>
2026-01-29 14:11:18 -0800

    syntax changes

--- cpp/include/rover_domain/core/agent/agent.hpp
+++ cpp/include/rover_domain/core/agent/agent.hpp
@@ -16,8 +16,20 @@ class DefaultAgent final : public IAgent {
     using SType = thyme::utilities::SharedWrap<SensorType>;
     using ActionType = std::vector<double>;
    public:
-    DefaultAgent(Bounds bounds, IndirectDifferenceParameters indirect_difference_parameters, std::string reward_type, std::string type_, double obs_radius = 1.0, SType sensor = SensorType())
-        : IAgent(bounds, indirect_difference_parameters, reward_type, type_, obs_radius), m_sensor(sensor) {}
+    DefaultAgent(
+        Bounds bounds,
+        IndirectDifferenceParameters indirect_difference_parameters,
+        std::string reward_type,
+        std::string type_,
+        double obs_radius = 1.0,
+        SType sensor = SensorType()
+    ) : IAgent(
+        bounds,
+        indirect_difference_parameters,
+        reward_type,
+        type_,
+        obs_radius
+    ), m_sensor(sensor) {}
     [[nodiscard]] virtual std::vector<double> scan(const Agents& agents, const POIs& pois, int agent_idx) const override {
         return m_sensor->scan(agents, pois, agent_idx);
     }
